c86992988fbf33e802d6990706021456d0469e8c
KhoirH <hilmi.khoirulloh02@gmail.com>
2022-03-27 15:23:57 +0700

    bugfix

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const app = express();
 const http = require('http').createServer(app);
 
 const io = require('socket.io')(http, {
-  allowRequest: (req, callback) => {
-      const noOriginHeader = req.headers.origin === undefined;
-      callback(null, noOriginHeader);
-  },
+  origin: "*",
   cors: {
     credentials: true
   },
