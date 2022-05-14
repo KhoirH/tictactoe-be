41dcfeb1cd2c431f3d20ba314ea66575524f9ce5
KhoirH <hilmi.khoirulloh02@gmail.com>
2022-05-14 16:07:29 +0700

    bugfix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const http = require('http').createServer(app);
 
 const io = require('socket.io')(http, {
-  origin: "*",
+  origin: 'https://khoirh.github.io',
   cors: {
     credentials: true
   },
